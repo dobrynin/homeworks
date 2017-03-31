75272e0b6cd4c5a250ad91eb0bf856afda4609ab
David Dobrynin <David.Dobrynin@gmail.com>
2017-03-30 22:07:28 -0700

    Write Map class code

--- W1D1/map.rb
+++ W1D1/map.rb
@@ -1,21 +1,37 @@
 class Map
   def initialize
-    # create ivar to store map here!
+    @map = []
   end
 
   def assign(key, value)
-    # assings a key-value pair to the map
+    @map.each do |existing_key, _, pair_index|
+      if key == existing_key
+        @map[pair_index] = [key, value]
+        return
+      end
+    end
+    @map.push([key, value])
   end
 
   def lookup(key)
-    # returns the value associated with the provided key
+    @map.each do |existing_key, value|
+      if key == existing_key
+        return value
+      end
+    end
+    nil
   end
 
   def remove(key)
-    # removes the key-value pair associated with the provided key
+    @map.each do |existing_key, value|
+      if key == existing_key
+        @map.delete([key, value])
+        return
+      end
+    end
   end
 
   def show
-    # return a copy of the map
+    @map
   end
 end
