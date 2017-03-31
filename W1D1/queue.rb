77fa6e13f76800cd8f97be86d7c1cc5088ab756d
David Dobrynin <David.Dobrynin@gmail.com>
2017-03-30 21:50:37 -0700

    Write Queue class code

--- W1D1/queue.rb
+++ W1D1/queue.rb
@@ -1,17 +1,17 @@
 class Queue
   def initialize
-    # create ivar to store queue here!
+    @queue = []
   end
 
   def enqueue(el)
-    # adds an element to the queue
+    @queue.push(el)
   end
 
   def dequeue
-    # removes one element from the queue
+    @queue.shift
   end
 
   def show
-    # return a copy of the queue
+    @queue
   end
 end
