06f68236ff28c963a3dad54d8a2008c643b41c60
David Dobrynin <David.Dobrynin@gmail.com>
2017-03-30 21:48:14 -0700

    Write Queue class framework

---
+++ W1D1/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+  def initialize
+    # create ivar to store queue here!
+  end
+
+  def enqueue(el)
+    # adds an element to the queue
+  end
+
+  def dequeue
+    # removes one element from the queue
+  end
+
+  def show
+    # return a copy of the queue
+  end
+end
