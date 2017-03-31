d784c2ba04c5e699a1583c801cc2ca15be657758
David Dobrynin <David.Dobrynin@gmail.com>
2017-03-30 21:53:46 -0700

    Write Map class framework

---
+++ W1D1/map.rb
@@ -0,0 +1,21 @@
+class Map
+  def initialize
+    # create ivar to store map here!
+  end
+
+  def assign(key, value)
+    # assings a key-value pair to the map
+  end
+
+  def lookup(key)
+    # returns the value associated with the provided key
+  end
+
+  def remove(key)
+    # removes the key-value pair associated with the provided key
+  end
+
+  def show
+    # return a copy of the map
+  end
+end
